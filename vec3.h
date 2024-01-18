2543b74faaedb190ee69235f289a9d2ea0b4b789
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-18 14:03:35 -0500

    first commit

---
+++ vec3.h
@@ -0,0 +1,21 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+    public:
+    float e[3];
+
+    vec3() : e{0,0,0} {};
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
+
+    double x() const {return e[0];};
+    double y() const {return e[1];};
+    double z() const {return e[2];};
+
+};
+
