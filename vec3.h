2c51f6ec478a62bbcfbfd6e115b86eaaeed16615
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-18 23:24:06 -0500

    Finished vector class header file

--- vec3.h
+++ vec3.h
@@ -13,9 +13,86 @@ class vec3 {
     vec3() : e{0,0,0} {};
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
 
-    double x() const {return e[0];};
-    double y() const {return e[1];};
-    double z() const {return e[2];};
+    double x() const {return e[0];};  //Get x coordinate
+    double y() const {return e[1];};  //Get y coordinate
+    double z() const {return e[2];};  //Get z coordinate
 
+    vec3 operator-() const{ return vec3(-e[0], -e[1], -e[2]); }; //Overloaded - operator to flip the sign of the vectors coordinates
+    double operator[](int i) const {return e[i];};
+    double& operator[](int i) const {return e[i];};
+
+    vec3 operator+=(const vec3& v){    //Overloaded = operator to add vectors
+        e[0] += v.e[0];
+        e[1] += v.e[1];
+        e[2] += v.e[2];
+        return *this;
+    }
+
+    vec3 operator*=(double t){        //Overloaded * operator to scale vectors
+        e[0] *= t;
+        e[1] *= t;
+        e[2] *= t;
+        return *this;
+    }
+
+    vec3 operator/=(double t){        //Overloaded / operator to scale vectors
+        return *this *= 1/t;
+    }
+
+    double length_squared() const {
+        return (e[0]*e[0]) + (e[1]*e[1]) + (e[2]*e[2]);
+    }
+
+    double length() const {                            //Caclulate vector length
+        return sqrt(length_squared());
+    }
+
+    // point3 is just an alias for vec3, but useful for geometric clarity in the code
+    using point3 = vec3;
+
+    // Vector Utility Functions
+    inline std::ostream& operator<<(std::ostream& out, const vec3& v) {
+        return out v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    }
+
+    inline vec3 operator+(const vec3& u, const vec3& v) {
+        return (u.e[0] + v.e[0]), (u.e[1] + v.e[1]), (u.e[2] + v.e[2]);
+    }
+
+    inline vec3 operator-(const vec3& u, const vec3& v) {
+        return (u.e[0] - v.e[0]), (u.e[1] - v.e[1]), (u.e[2] - v.e[2]);    
+    }
+
+    inline vec3 operator*(const vec3& u, const vec3& v) {
+        return (u.e[0] * v.e[0]), (u.e[1] * v.e[1]), (u.e[2] * v.e[2]);    
+    }
+
+    inline vec3 operator*(double t, const vec3& v) {
+        return (v.e[0] * t), (v.e[1] * t), (v.e[2] * t);    
+    }
+
+    inline vec3 operator*(double t, const vec3& v) {
+        return (v * t);    
+    }
+
+    inline vec3 operator*(double t, const vec3& v) {
+        return (1/t) * v;    
+    }
+
+    inline double dot(const vec3& u, const vec3& v) {
+        return (u.e[0] + v.e[0]) + (u.e[1] + v.e[1]) + (u.e[2] + v.e[2]);
+    }
+
+    inline vec3 cross(const vec3& u, const vec3& v) {
+        return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+    }
+
+    inline vec3 unit_vector(const vec3& v) {
+        return v / v.length();
+    }
+
+#endif
 };
 
