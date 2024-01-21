1f1e53b911deda08773c20502475e3415a3c30a2
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-21 11:36:29 -0500

    Added viewport and color gradient on vector.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
 
-int main() {
-    // Image
+color ray_color(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1);
+    return (1-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
-    int image_width = 256;
-    int image_height = 256;
+int main() {
+    // Ray Frame
+    auto aspect_ratio = 16/9;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);   //Cast to int.
+    if (image_height < 1){
+        image_height = 1;
+    }
+
+    //Camera + Viewport height settings.
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = (double(image_height)/image_height);
+    auto camera_center = point3(0,0,0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);   //Left edge to right edge
+    auto viewport_v = vec3(0, -viewport_height,0 );   //Upper edge to lower edge
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
-
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
