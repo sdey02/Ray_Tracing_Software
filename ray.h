0b36387f99b88f3ae9fcedf89189a994281628d7
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-21 11:35:59 -0500

    Updated ray.h header fine var name.

--- ray.h
+++ ray.h
@@ -7,16 +7,16 @@ class ray{
 
     private:
         // Ray as a function: P(t) = A + tB  | A is the origin of ray and B is direction of ray 
-        point3 origin;
+        point3 orig;
         vec3 dir;
 
     public:
-        ray(const point3& origin, const vec3& direction) : origin(origin), dir(direction) {}
-        const point3& origin() const {return origin;}
+        ray(const point3& origin, const vec3& direction) : orig(origin), dir(direction) {}
+        const point3& origin() const {return orig;}
         const vec3& direction() const {return dir;}
 
         point3 at(double t) const {
-            return origin + t*dir;
+            return orig + t*dir;
         }
 };
 
