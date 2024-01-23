0b2f0b4434f8830e5c2d2b387436e40722b54082
Shreyans <78007563+sdey02@users.noreply.github.com>
2024-01-23 18:07:22 -0500

    Calculates if we are hitting the sphere

--- main.cpp
+++ main.cpp
@@ -3,7 +3,27 @@
 #include "ray.h"
 #include "vec3.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto h = dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = h*h - a*c;
+    
+    if (discriminant < 0){
+        return -1.0;
+    } else {
+        return (h - sqrt(discriminant))/ a;
+    }
+}
+
 color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1);
     return (1-a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
